b05cf7d5815f787d52a9f77a14f74ee32fff07ab
kagemusha <mmadrid99@gmail.com>
2013-09-16 22:54:46 -0400

    change namespace

--- app/app.js
+++ app/app.js
@@ -3,14 +3,14 @@ import Resolver from 'resolver';
 var App = Ember.Application.create({
   LOG_ACTIVE_GENERATION: true,
   LOG_VIEW_LOOKUPS: true,
-  modulePrefix: 'appkit',
+  modulePrefix: 'kids-math',
   Resolver: Resolver
 });
 
-import hbsInitializer from 'appkit/initializers/handlebars';
+import hbsInitializer from 'kids-math/initializers/handlebars';
 Ember.Application.initializer(hbsInitializer);
 
-import routes from 'appkit/routes';
+import routes from 'kids-math/routes';
 App.Router.map(routes);
 
 export default App;
